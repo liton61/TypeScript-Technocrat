283d11db0f1e2ea028ac1e48f60b7f82066408f5
liton61 <litonofficial61@gmail.com>
2024-07-28 21:43:35 +0600

    type alias added

---
+++ src/ts/typeAlias.ts
@@ -0,0 +1,15 @@
+type Info = {
+  id: string;
+  name: string;
+  address: string;
+  contact: string;
+  country: string;
+};
+
+const oldUser: Info = {
+  id: "1",
+  name: "Liton",
+  address: "Narsingdi",
+  contact: "017",
+  country: "Bangladesh",
+};
